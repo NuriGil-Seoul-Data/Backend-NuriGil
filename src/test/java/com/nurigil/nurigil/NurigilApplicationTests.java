3fe490bd9f433e0a32841e4cd192519796be6749
bingseok <qqq7384@naver.com>
2025-04-07 18:47:17 +0900

    first commit

---
+++ src/test/java/com/nurigil/nurigil/NurigilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurigil.nurigil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurigilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
